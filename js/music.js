603b14cff22eb6e1e75fcc30fdcfbaa431469170
cainff <germainkav@gmail.com>
2016-12-01 15:53:27 +0100

    work on btn dynamic for player

--- js/music.js
+++ js/music.js
@@ -1,6 +1,6 @@
-/**Button test**/
+/**Button test for playlist menu**/
 
-function playlist(){
+function toggle(){
     if ($('#tracks').hasClass('hide_')){
         $('#tracks').removeClass('hide_');
     } else {    
@@ -8,63 +8,42 @@ function playlist(){
     }
 };
 
-
-var music=document.getElementById('btn_test');
-    music.addEventListener('click',playlist); 
-
-
-
-/*global jQuery:true, playerjs:true, $Player:true */
-(function($, document, window){
-
-  var booba = [
-    'https://www.youtube.com/watch?v=H_AbU24v4EU&list=TLGG-MN34pVPZgMyOTExMjAxNg',                  
-      'https://www.youtube.com/watch?v=NaR2l0JXQas',
-    'https://www.youtube.com/watch?v=-KRe61NpaTA',
-    'https://www.youtube.com/watch?v=9GJJMZjISmY',
-    'https://www.youtube.com/watch?v=eh17foTBGmg',
-    'https://www.youtube.com/watch?v=Bz2YJ7yyuL8',
-  ];
-
-  var $tracks = $('#tracks'),
+var $tracks,
     players = [],
     count = 0,
     isReady = false;
 
-  var start = function(){
-    var index = 0;
+var playlists = {
+    booba : [
+        'https://www.youtube.com/watch?v=Stet_4bnclk',                  
+        'https://www.youtube.com/watch?v=Tk20thb6z-g',
+        'https://www.youtube.com/watch?v=vq1ztJsbRek',
+        'https://www.youtube.com/watch?v=NaR2l0JXQas',
+        'https://www.youtube.com/watch?v=wa1jFkDejVE',
+        'https://www.youtube.com/watch?v=hwtLqJyhS3Q'
+    ],
+    
+    rohff : [
+        'https://www.youtube.com/watch?v=s9Ye9I1NZTE',                  
+        'https://www.youtube.com/watch?v=TIyi-CPR-jE',
+        'https://www.youtube.com/watch?v=HOmP295oDMU',
+        'https://www.youtube.com/watch?v=PMShE8Io5jQ',
+        'https://www.youtube.com/watch?v=rGpz8okD7SM',
+        'https://www.youtube.com/watch?v=I-roQNycZn8'
+    ]
+}
 
-    var multi = new $Player(players);
-
-    // Set the callout.
-    multi.on('active', function(index){
-      $('.panel').removeClass('callout').eq(index).addClass('callout');
-    });
-
-    // Go to a track by clicking on it.
-    $('.panel').on('click', function(){
-      if (!isReady){
-        return false;
-      }
-      var index = $('.panel').index(this);
-      multi.play(index);
-      return false;
-    });
-
-    isReady = true;
-  };
-
-  // We need to wait for all the players to be ready before we go.
-  var onReady = function(){
+var onReady = function(){
     count++;
-    if (count === booba.length){
+    if (count === playlists.rohff.length){
       start();
     }
-  };
+};
 
-  $(document).on('ready', function(){
-    // Go get all the URLS from embedly and then embed them.
-    $.embedly.oembed(booba, {query:{better: true} })
+
+var fill = function(pls){
+    console.log('fill');
+    $.embedly.oembed(pls, {query:{better: true} })
       .done(function(results){
         $.each(results, function(i, obj){
           $tracks.append(['<li class="track">',
@@ -93,14 +72,50 @@ var music=document.getElementById('btn_test');
           });
         });
       });
+}
 
+var start = function(){
+    var index = 0;
+
+    var multi = new $Player(players);
+
+    // Set the callout.
+    multi.on('active', function(index){
+      $('.panel').removeClass('callout').eq(index).addClass('callout');
+    });
+
+    // Go to a track by clicking on it.
+    $('.panel').on('click', function(){
+      if (!isReady){
+        return false;
+      }
+      var index = $('.panel').index(this);
+      multi.play(index);
+      return false;
+    });
+
+    isReady = true;
+};
+
+/*global jQuery:true, playerjs:true, $Player:true */
+(function($, document, window){
+  $tracks = $('#tracks');
+
+  
+
+  // We need to wait for all the players to be ready before we go.
+  
+  $(document).on('ready', function(){
+    // Go get all the URLS from embedly and then embed them.
+    
+      //fill(playlists.rohff);
   });
-
 })(jQuery, document, window);
 
-$("#btn_test2" ).click(function() {
-    $("ul").empty();
-    
+$('.fillply' ).click(function() {
+    toggle();
+    $('ul#tracks').empty();
+    fill(playlists[$(this).attr('data-playlist')]);
 });
 
 
